5f96d49e8595936310437cabb419738f578fea54
CHerobrine <cherobrine2015@gmail.com>
2017-01-25 17:51:10 -0500

    new and improved

--- DodgeDaDuck/Game.py
+++ DodgeDaDuck/Game.py
@@ -4,14 +4,21 @@ from pygame.locals import *
 import random
 import time
 import sys
+from random import randint
 from stem_airplane import*
+print('loading files')
+print('loading complete starting game')
+
+
+
 class player(pygame.sprite.Sprite):
-    
+
     def __init__(self):
         super(player, self).__init__()
         self.image=pygame.image.load('biplane.png').convert()
         self.image.set_colorkey((255,255,255),RLEACCEL)
         self.rect=self.image.get_rect()
+    
         
     def update(self,pressed_keys):
         if pressed_keys[K_UP]:
@@ -31,7 +38,8 @@ class player(pygame.sprite.Sprite):
             self.rect.top=0
         elif self.rect.bottom>=600:
             self.rect.bottom=600
-
+        if pressed_keys[K_SHIFT]:
+            player.kill
 
 class opponent(pygame.sprite.Sprite):
     #def __init__(self):
@@ -43,6 +51,7 @@ class opponent(pygame.sprite.Sprite):
     def __init__(self):
         super(opponent,self).__init__()
         self.image=pygame.image.load('Duck.jpg')
+        
         self.image.set_colorkey((255,255,255),RLEACCEL)
         self.rect=self.image.get_rect(
             center=(random.randint(850,900),random.randint(0,600))
@@ -52,7 +61,7 @@ class opponent(pygame.sprite.Sprite):
         self.rect.move_ip(-self.speed,0)
         if self.rect.right<0:
             self.kill()
-class FIRE(pygame.sprite.Sprite):
+class meteor(pygame.sprite.Sprite):
     #def __init__(self):
       #  super(opponent,self).__init__()
        # self.surf=pygame.Surface((20,10))
@@ -60,18 +69,18 @@ class FIRE(pygame.sprite.Sprite):
        # self.rect=self.surf.get_rect(center=(820,random.randint(0,600)))
        # self.speed=random.randint(0,2)
     def __init__(self):
-        super(opponent,self).__init__()
-        self.image=pygame.image.load('FIRE.png')
+        super(meteor,self).__init__()
+        self.image=pygame.image.load('meteor.png')
         self.image.set_colorkey((255,255,255),RLEACCEL)
         self.rect=self.image.get_rect(
             center=(random.randint(850,900),random.randint(0,600))
             )
-        self.speed = random.randint(0,1)
+        self.speed = random.randint(1,9)
     def update(self):
         self.rect.move_ip(-self.speed,0)
         if self.rect.right<0:
             self.kill()
-            
+                        
 
 class Cloud(pygame.sprite.Sprite):
     def __init__(self,color):
@@ -104,7 +113,6 @@ class Rain(pygame.sprite.Sprite):
         if self.rect.bottom<900:
             self.kill()
     
-        #create level function
 def set_level(level):
     caption = 'Airplane - ' + ' Level ' + str(level)
     pygame.display.set_caption(caption)
@@ -114,6 +122,7 @@ def set_level(level):
         
 
 pygame.init()
+my_font=pygame.font.SysFont("helvetica",16)
 screen=pygame.display.set_mode((800,600))
 player = player()
 background=pygame.Surface(screen.get_size())
@@ -121,7 +130,6 @@ background.fill((0,122,224))
 
 players=pygame.sprite.Group
 opponents=pygame.sprite.Group()
-FIRE=pygame.sprite.Group()
 all_sprites=pygame.sprite.Group()
 all_sprites.add(player)
 ADDOPPONENT=pygame.USEREVENT+1
@@ -130,10 +138,13 @@ pygame.time.set_timer(ADDOPPONENT,250)
 #surf.fill((200,0,1))
 #rect=surf.get_rect()
 running = True
+
 clock=pygame.time.Clock()
-fps=1000
+fps= 1000
+score = 10
 while running:
     clock.tick(fps)
+    scoretext=my_font.render(str(pygame.time.get_ticks()/100),1,(0,0,0))
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -144,7 +155,14 @@ while running:
             new_opponent=opponent()
             opponents.add(new_opponent)
             all_sprites.add(new_opponent)
+            rnum=randint(0,6)
+            if rnum==0:
+                new_meteor=meteor()
+                opponents.add(new_meteor)
+                all_sprites.add(new_meteor)
+
     screen.blit(background,(0,0))
+    screen.blit(scoretext,(5,10))
     pressed_keys=pygame.key.get_pressed()
     player.update(pressed_keys)
     opponents.update()
@@ -153,8 +171,10 @@ while running:
     #screen.blit(player.surf,player.rect)
     for entity in all_sprites: 
          screen.blit(entity.image, entity.rect)
+         
     if pygame.sprite.spritecollideany(player,opponents):
         player.kill()
+        
     pygame.display.flip()
 
 
---
+++ DodgeDaDuck/mr medic drawer thingy.py
@@ -0,0 +1,11 @@
+from turtle import*
+shape('turtle')
+right(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+done()
